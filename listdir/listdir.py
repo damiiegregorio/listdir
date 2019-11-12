b8e1c4a488589d34dca5b0bfa4c7583f6a6c956b
damiiegregorio <damiiekeith@gmail.com>
2019-11-12 16:41:49 +0800

    Add files via upload

--- listdir/listdir.py
+++ listdir/listdir.py
@@ -11,10 +11,37 @@ import getpass
 import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import pika
+
+yaml_file = 'config.yaml'
+
+
+def publish_queue(file_path):
+    global yaml_file
+    with open(yaml_file, 'rt') as f:
+        config = yaml.safe_load(f.read())
+    rabbit_mq = config['rabbitmq']
+
+    # Create a new instance of the Connection object
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_mq['host']))
+    channel = connection.channel()
+    channel.queue_declare(queue=rabbit_mq['queue'])
+    json_data = json_file(file_path)
+    data = json_data['files']
+    for message in data:
+        channel.basic_publish(exchange='',
+                              routing_key='listdir',
+                              body=json.dumps(message),
+                              properties=pika.BasicProperties(
+                                  delivery_mode=2,
+                              ))
+        logger.info("Sent {}".format(message))
+    connection.close()
 
 
 def create_db():
-    with open("config.yaml", 'rt') as f:
+    global yaml_file
+    with open(yaml_file, 'rt') as f:
         config = yaml.safe_load(f.read())
     mysql = config['mysql']
 
@@ -37,7 +64,8 @@ def create_db():
 
 def create_table():
     try:
-        with open("config.yaml", 'rt') as f:
+        global yaml_file
+        with open(yaml_file, 'rt') as f:
             config = yaml.safe_load(f.read())
         mysql = config['mysql']
 
@@ -74,7 +102,8 @@ def create_table():
 
 def list_to_db(file_path):
     try:
-        with open("config.yaml", 'rt') as f:
+        global yaml_file
+        with open(yaml_file, 'rt') as f:
             config = yaml.safe_load(f.read())
         mysql = config['mysql']
 
@@ -84,53 +113,45 @@ def list_to_db(file_path):
             password=pwd,
             host=mysql['host'],
             port=mysql['port'],
-            # database=mysql['db'],
         )
 
         cursor = connection.cursor()
-        logger.info("Connected to database.")
         cursor.execute("select exists(SELECT datname FROM pg_catalog.pg_database WHERE datname = '{}');".format(mysql['db']))
         db_exists = cursor.fetchone()[0]
         if db_exists is False:
             logger.info("Database does not exist.")
             logger.info("Please make a database using the '-d' command")
         else:
-            cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", ('files',))
-            table_exists = cursor.fetchone()[0]
-            logger.info(table_exists)
-            if table_exists is True:
-                logger.info("Table does not exist. Enter your password to create a table.")
-            else:
-                logger.info("Preparing your data...")
-                connection = psycopg2.connect(
-                    user=mysql['user'],
-                    password=pwd,
-                    host=mysql['host'],
-                    port=mysql['port'],
-                    database=mysql['db'],
-                )
-                cursor = connection.cursor()
+            logger.info("Preparing your data...")
+            connection = psycopg2.connect(
+                user=mysql['user'],
+                password=pwd,
+                host=mysql['host'],
+                port=mysql['port'],
+                database=mysql['db'],
+            )
+            cursor = connection.cursor()
 
-                for r, d, f in os.walk(file_path):
-                    for file in f:
-                        dir_path = os.path.abspath(r)
-                        name = os.path.basename(file)
-                        file_stat = os.path.getsize("{}{}{}".format(r, os.sep, file))
-                        hash_path = os.path.abspath("{}{}{}".format(r, os.sep, file))
-                        sha1 = extract_sha1(hash_path)
-                        md5 = extract_md5(hash_path)
-                        data = [dir_path, name, file_stat, sha1, md5]
-                        logger.debug(data)
+            for r, d, f in os.walk(file_path):
+                for file in f:
+                    dir_path = os.path.abspath(r)
+                    name = os.path.basename(file)
+                    file_stat = os.path.getsize("{}{}{}".format(r, os.sep, file))
+                    hash_path = os.path.abspath("{}{}{}".format(r, os.sep, file))
+                    sha1 = extract_sha1(hash_path)
+                    md5 = extract_md5(hash_path)
+                    data = [dir_path, name, file_stat, sha1, md5]
+                    logger.debug(data)
 
-                        postgres_insert_query = """
-                            INSERT INTO files
-                            (PATH, FILENAME, SIZE, SHA1, MD5)
-                            VALUES (%s,%s,%s,%s,%s)
-                        """
-                        record_to_insert = (dir_path, name, file_stat, sha1, md5)
-                        cursor.execute(postgres_insert_query, record_to_insert)
-                        connection.commit()
-                        logger.info("Record inserted successfully into file table")
+                    postgres_insert_query = """
+                        INSERT INTO files
+                        (PATH, FILENAME, SIZE, SHA1, MD5)
+                        VALUES (%s,%s,%s,%s,%s)
+                    """
+                    record_to_insert = (dir_path, name, file_stat, sha1, md5)
+                    cursor.execute(postgres_insert_query, record_to_insert)
+                    connection.commit()
+                    logger.info("Record inserted successfully into file table")
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
@@ -145,14 +166,14 @@ def create_all():
 
 def setup_yaml():
     """Setup logging configuration """
+    global yaml_file
     default_level = logging.DEBUG
-    yaml_path = 'config.yaml'
     value = os.getenv("LOG_CFG", None)
 
     if value:
-        yaml_path = value
-    if os.path.exists(yaml_path):
-        with open(yaml_path, 'rt') as f:
+        yaml_file = value
+    if os.path.exists(yaml_file):
+        with open(yaml_file, 'rt') as f:
             try:
                 config = yaml.safe_load(f.read())
                 logging.config.dictConfig(config)
@@ -207,9 +228,10 @@ def json_file(file_path):
                     "sha1": sha1,
                     "md5": md5
                 }
-                logger.debug(data)
+                # logger.debug(data)
                 data_list.append(data)
         json.dump(data_dict, json_file, indent=2)
+    return data_dict
 
 
 def csv_write(file_path):
@@ -246,6 +268,7 @@ def main():
         group.add_argument("-c", "--csv", help="Create a csv file", action="store_true")
         group.add_argument("-w", "--write", help="Write to DB", action="store_true")
         group.add_argument("-d", "--create_db", help="Create database and table", action="store_true")
+        group.add_argument("-q", "--publish", help="Send to queue", action="store_true")
         args = parser.parse_args()
 
         if args.file_path is None and args.csv_name is None:
@@ -264,6 +287,9 @@ def main():
             list_to_db(args.file_path)
         elif args.create_db:
             create_all()
+        elif args.publish:
+            logger.info("Sending files metadata to listdir queue..")
+            publish_queue(args.file_path)
         else:
             logger.info("Creating csv file...")
             csv_write(args.file_path)
